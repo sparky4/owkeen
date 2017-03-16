521405e07d1762ce516d2e3ecbcb82754458006e
sparky4 <sparky4@cock.li>
2017-03-16 15:36:58 -0500

    Init Commit

---
+++ awoo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#for testing
+#mv src/WOLF3D.EXE ./wolf3d.exe
+#. ./movedataout.sh
+if [ -z "$*" ]; then
+		pee="wwww"
+	else
+		pee="$*"
+fi
+git add .
+. ./boop.sh "$pee"
+#. ./movedatain.sh
---
+++ wbuild.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm *.obj
+#wmake -f mk.mak -h clean
+#wmake -f mk.mak -h
+wmake -h clean
+wmake -h
+#wmake -h vomitchan
